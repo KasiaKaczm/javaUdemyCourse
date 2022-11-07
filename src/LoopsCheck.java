4abdb64d9ea6998f64b224570d8af4771af1c6b8
Katarzyna Kaczmarek <katarzynakaczmarek392@gmail.com>
2022-11-07 15:38:32 +0100

    add Auto and AutoTest

--- src/LoopsCheck.java
+++ src/LoopsCheck.java
@@ -20,6 +20,7 @@ public class LoopsCheck {
             int temp = numbers[i]; //dla i=0 1 i=1 3
             numbers[i] = numbers[numbers.length - 1 - i]; //4 ->0 3->7
             numbers[numbers.length - 1 - i] = temp; // 1  // numbers[3]=3
+            System.out.println("Iteracja numer " + i);
         }
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
