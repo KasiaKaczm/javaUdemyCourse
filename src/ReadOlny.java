499dcf68d75aa8aba7c215ef8fcd3ebdd3a804de
Katarzyna Kaczmarek <katarzynakaczmarek392@gmail.com>
2022-11-24 13:40:20 +0100

    add excersices about getter and setter

---
+++ src/ReadOlny.java
@@ -0,0 +1,24 @@
+public class ReadOlny {
+
+    private String name=" Kasia ";
+
+    //GETTER
+  /*  public String getName(){
+        return name = name + "jest ok";
+    }*/
+
+    //SETTER
+
+   /* public void setName(String name) {
+
+        this.name = name;*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+}
