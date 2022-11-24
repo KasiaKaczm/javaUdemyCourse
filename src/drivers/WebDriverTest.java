91ddf5407a485020ce4420b8d2eb3a67ca5517fc
Katarzyna Kaczmarek <katarzynakaczmarek392@gmail.com>
2022-11-24 15:30:32 +0100

    add excercises about polymorphism connected with changes in code (WebDriverTest java class)

--- src/drivers/WebDriverTest.java
+++ src/drivers/WebDriverTest.java
@@ -3,12 +3,30 @@ package drivers;
 public class WebDriverTest {
     public static void main(String[] args) {
 
-        ChromeDriver chrome= new ChromeDriver();
-        chrome.get();
-        chrome.findElementBy();
+        WebDriver driver = getDriver("firefox");
+        driver.get();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
 
-        FirefoxDriver firefox=new FirefoxDriver();
+      /*  FirefoxDriver firefox=new FirefoxDriver();
                 firefox.get();
         firefox.findElementBy();
+        chrome.findElementBy();
+        chrome.findElementBy();
+        chrome.findElementBy();
+        chrome.findElementBy();
+        chrome.findElementBy();*/
+    }
+
+    private static WebDriver getDriver(String name) {
+        if(name.equals("chrome")){
+            return new ChromeDriver();
+        } else if (name.equals("firefox")){
+            return new FirefoxDriver();
+        }
+        return null;
     }
 }
